0ef4d1d0d4d31bfd6acf8c66e9b8a2745c18aa83
kawasakiMak <gankstargamer@gmail.com>
2024-08-22 13:17:50 +0500

    added TaskController code

--- src/main/java/kz/com/task/tracker/api/controllers/TaskController.java
+++ src/main/java/kz/com/task/tracker/api/controllers/TaskController.java
@@ -1,4 +1,29 @@
 package kz.com.task.tracker.api.controllers;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "task")
 public class TaskController {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+
+    String name;
+
+    @Builder.Default
+    Instant createdAt = Instant.now();
+
+    String description;
 }
